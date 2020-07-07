d1200fbd588f208805578b3d03a9180b75fd76f2
BanKai <leagueoflegends.talk@gmail.com>
2020-07-07 22:07:36 +0300

    create admin repaire controllers index

---
+++ app/controllers/admin/repairers_controller.rb
@@ -0,0 +1,25 @@
+class Admin::RepairersController < ApplicationController
+ 
+ def index
+ 	@repairers = Repairer.all 
+ end
+
+ def create
+ end
+
+ def new
+ end
+
+ def edit
+ end
+
+ def show
+ end
+
+ def update
+ end
+
+ def destroy
+ end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   devise_for :admins
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :friends
+  resources :repairers, only: 'index'
+
+  namespace :admin do
+  	resources :repairers
+  end
 end
