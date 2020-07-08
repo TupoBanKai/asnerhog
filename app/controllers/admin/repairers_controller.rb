3322040381ba7b26fd507ca868b3040d7546a72a
BanKai <leagueoflegends.talk@gmail.com>
2020-07-08 20:07:37 +0300

    action controller Repairers

--- app/controllers/admin/repairers_controller.rb
+++ app/controllers/admin/repairers_controller.rb
@@ -5,21 +5,33 @@ class Admin::RepairersController < ApplicationController
  end
 
  def create
+	Repairer.create(params[:repairers])
+	#переход на другое действие контроллера
+	redirect_to admin_repairers_path
  end
 
  def new
+ 	@rep = Repairer.new
  end
 
  def edit
+ 	@rep = Repairer.find(params[:id])
  end
 
  def show
+ 	@rep = Repairer.find(params[:id])
  end
 
  def update
+ 	ye = Repairer.find(params[:id]
+ 	ye.update(params[:repairers])
+ 	redirect_to admin_repairers_path
  end
 
  def destroy
+ 	ye = Repairer.find(params[:id]
+ 	ye.delete
+ 	redirect_to admin_repairers_path
  end
 
 end
